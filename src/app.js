8751cfbfd07995f9eab66692c8a06020dc3653ea
unknown <info@johnnyramirez.es>
2022-11-10 10:05:30 +0100

    Declaración de variables

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const harina="Harina integral, de centeno";
+const agua="Agua";
+const aceite="Aceite";
+const sal="Sal";
+const levadura="Levadura de panadería";
+const azucar="Azúcar";
+const salsa="Salsa de tomate";
+const queso="Mozzarella";
+const pepperoni="Pepperoni";
+const aceitunas="Aceitunas";
+const champinones="Chanpiñones";
+
