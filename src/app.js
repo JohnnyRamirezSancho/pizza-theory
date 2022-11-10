65de43df1b47db6341f5889c30973b08678ba1f7
unknown <info@johnnyramirez.es>
2022-11-10 11:51:00 +0100

    Añadiendo funciones prepararLevadura y prepararMasa

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-const harina="Harina integral, de centeno";
+const harina="Harina integral de centeno";
 const agua="Agua";
+const aguaTemplada="Agua templada";
 const aceite="Aceite";
 const sal="Sal";
 const levadura="Levadura de panadería";
@@ -10,3 +11,19 @@ const pepperoni="Pepperoni";
 const aceitunas="Aceitunas";
 const champinones="Chanpiñones";
 
+let harinaPeso="400 grs";
+let aguaPeso="100 grs";
+let aguaTempladaPeso="100 grs";
+let aceitePeso="50 grs";
+let salPeso="1 cucharadita de café";
+let levaduraPeso="16 grs";
+let azucarPeso="1 cucharadita de café";
+
+function prepararLevadura(){
+    console.log("Mezclar " + aguaTempladaPeso + " de " + aguaTemplada + ", el " + azucar + " y la " + levadura +"."); 
+}
+ function prepararMasa(){
+    console.log("Mezclar la " + harina + ", el " + aceite + " y la " + sal + "."); 
+ }
+
+prepararLevadura();
